d4c1b6eb8c9ef79e7a0083f37df38be94cbfff68
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-18 06:58:15 -0800

    update portfolio
    new design

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -7,42 +7,6 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
-//Displays first image in slideshow on page load
-var slideIndex = 1;
-showSlides(slideIndex);
-
-//Next and previous buttons slide change
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-//Changes slides when dots are clicked
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    //stores mySlides elements in variable array "slides"
-    var slides = document.getElementsByClassName("mySlides");
-    //stores dot elements in variable array "dots"
-    var dots = document.getElementsByClassName("dot");
-    //if value given to n is greater than amount of slides inex is 1
-    if (n > slides.length) {slideIndex = 1};
-    //if value given to n is less than 1 slide index is slides length
-    if (n < 1) {slideIndex = slides.length};
-    //loop goes through array and sets slide display to none
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    //loop goes through array 'dots' and removes 'active' styling 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    //displays the image in the slideshow
-    slides[slideIndex - 1].style.display = "block";
-    //applies active styling to dot associated with image
-    dots[slideIndex - 1].className += " active";
-}
 
 //creates the contact form when user clicks off item
 //add evenetlistener for 'clicks'
@@ -53,3 +17,26 @@ document.addEventListener("click", function(event) {
         closeForm()
     }
 }, false )
+
+
+// function to display labels on hover
+function toggleLabelOnHover(buttonId, labelId) {
+    var button = document.getElementById(buttonId);
+    var label = document.getElementById(labelId);
+
+    button.addEventListener('mouseenter', function () {
+        label.style.display = 'block';
+    });
+
+    button.addEventListener('mouseleave', function () {
+        label.style.display = 'none';
+    });
+}
+
+
+// Apply the hover effect for each button-label pair
+toggleLabelOnHover('htmlButton', 'htmlLabel');
+toggleLabelOnHover('dbButton', 'dbLabel');
+toggleLabelOnHover('pythonButton', 'pythonLabel');
+toggleLabelOnHover('csharpButton', 'csharpLabel');
+toggleLabelOnHover('dotNetButton', 'dotNetLabel');
